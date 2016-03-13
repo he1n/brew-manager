0316054714ed720b2db9d53d161ce57dcc8a7997
he1n <caule.simon@gmail.com>
2016-03-13 21:12:23 +0000

    solve lcd bug and start main code for raspberry

--- brew-pi-controller/lcd.py
+++ brew-pi-controller/lcd.py
@@ -1,64 +1,86 @@
 #!/usr/bin/python
+
+#import
+import RPi.GPIO as GPIO
+import time
+
 class Lcd:
+    def __init__(self):
+        self.LCD_RS = 7
+        self.LCD_E  = 8
+        self.LCD_D4 = 25
+        self.LCD_D5 = 24
+        self.LCD_D6 = 23
+        self.LCD_D7 = 18
+        self.LCD_WIDTH = 16    # Maximum characters per line
+        self.LCD_CHR = True
+        self.LCD_CMD = False
+        self.LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+        self.LCD_LINE_2 = 0xC0
+        self.E_PULSE = 0.0005
+        self.E_DELAY = 0.0005
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+        GPIO.setup(self.LCD_E, GPIO.OUT)  # E
+        GPIO.setup(self.LCD_RS, GPIO.OUT) # RS
+        GPIO.setup(self.LCD_D4, GPIO.OUT) # DB4
+        GPIO.setup(self.LCD_D5, GPIO.OUT) # DB5
+        GPIO.setup(self.LCD_D6, GPIO.OUT) # DB6
+        GPIO.setup(self.LCD_D7, GPIO.OUT) # DB7
+        self.lcd_init()
+        
+    def lcd_init(self):
+        self.lcd_byte(0x33,self.LCD_CMD) # 110011 Initialise
+        self.lcd_byte(0x32,self.LCD_CMD) # 110010 Initialise
+        self.lcd_byte(0x06,self.LCD_CMD) # 000110 Cursor move direction
+        self.lcd_byte(0x0C,self.LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+        self.lcd_byte(0x28,self.LCD_CMD) # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01,self.LCD_CMD) # 000001 Clear display
+        time.sleep(self.E_DELAY)
 
-    def __init__(self, pin_rs=7, pin_e=8, pins_db=[25, 24, 23, 18]):
+    def lcd_byte(self,bits, mode):
+        GPIO.output(self.LCD_RS, mode)
+        GPIO.output(self.LCD_D4, False)
+        GPIO.output(self.LCD_D5, False)
+        GPIO.output(self.LCD_D6, False)
+        GPIO.output(self.LCD_D7, False)
+        if bits&0x10==0x10:
+            GPIO.output(self.LCD_D4, True)
+        if bits&0x20==0x20:
+            GPIO.output(self.LCD_D5, True)
+        if bits&0x40==0x40:
+            GPIO.output(self.LCD_D6, True)
+        if bits&0x80==0x80:
+            GPIO.output(self.LCD_D7, True)
+        self.lcd_toggle_enable()
+        GPIO.output(self.LCD_D4, False)
+        GPIO.output(self.LCD_D5, False)
+        GPIO.output(self.LCD_D6, False)
+        GPIO.output(self.LCD_D7, False)
+        if bits&0x01==0x01:
+            GPIO.output(self.LCD_D4, True)
+        if bits&0x02==0x02:
+            GPIO.output(self.LCD_D5, True)
+        if bits&0x04==0x04:
+            GPIO.output(self.LCD_D6, True)
+        if bits&0x08==0x08:
+            GPIO.output(self.LCD_D7, True)
+        self.lcd_toggle_enable()
 
-        self.pin_rs=pin_rs
-        self.pin_e=pin_e
-        self.pins_db=pins_db
+    def lcd_toggle_enable(self):
+        # Toggle enable
+        time.sleep(self.E_DELAY)
+        GPIO.output(self.LCD_E, True)
+        time.sleep(self.E_PULSE)
+        GPIO.output(self.LCD_E, False)
+        time.sleep(self.E_DELAY)
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin_e, GPIO.OUT)
-        GPIO.setup(self.pin_rs, GPIO.OUT)
-        for pin in self.pins_db:
-            GPIO.setup(pin, GPIO.OUT)
-
-        self.clear()
-
-    def clear(self):
-        """ Blank / Reset LCD """
-
-        self.cmd(0x33) # $33 8-bit mode
-        self.cmd(0x32) # $32 8-bit mode
-        self.cmd(0x28) # $28 8-bit mode
-        self.cmd(0x0C) # $0C 8-bit mode
-        self.cmd(0x06) # $06 8-bit mode
-        self.cmd(0x01) # $01 8-bit mode
-
-    def cmd(self, bits, char_mode=False):
-        """ Send command to LCD """
-
-        sleep(0.001)
-        bits=bin(bits)[2:].zfill(8)
-
-        GPIO.output(self.pin_rs, char_mode)
-
-        for pin in self.pins_db:
-            GPIO.output(pin, False)
-
-        for i in range(4):
-            if bits[i] == "1":
-                GPIO.output(self.pins_db[::-1][i], True)
-
-        GPIO.output(self.pin_e, True)
-        GPIO.output(self.pin_e, False)
-
-        for pin in self.pins_db:
-            GPIO.output(pin, False)
-
-        for i in range(4,8):
-            if bits[i] == "1":
-                GPIO.output(self.pins_db[::-1][i-4], True)
-
-
-        GPIO.output(self.pin_e, True)
-        GPIO.output(self.pin_e, False)
-
-    def message(self, text):
-        """ Send string to LCD. Newline wraps to second line"""
-
-        for char in text:
-            if char == '\n':
-                self.cmd(0xC0) # next line
-            else:
-                self.cmd(ord(char),True)
+    def lcd_string(self,message,line):
+        if(line == 1):
+            linetowrite = self.LCD_LINE_1
+        else:
+            linetowrite = self.LCD_LINE_2
+        message = message.ljust(self.LCD_WIDTH," ")
+        self.lcd_byte(linetowrite, self.LCD_CMD)
+        for i in range(self.LCD_WIDTH):
+            self.lcd_byte(ord(message[i]),self.LCD_CHR)
--- brew-pi-controller/main.py
+++ brew-pi-controller/main.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-from time import sleep
-import lcd
-import temperatureSensor
+import os
+import time
+from lcd import Lcd
+from temperatureSensor import TemperatureSensor
+
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
 
 if __name__ == "__main__":
     #Initialize lcd and test screen 
-
+    lcd = Lcd()
+    
     #Get all sensor name 
-
-    #Initialize all temperatureSensor and check if each exist on raspberry pi and if we have new sensor 
-
-	    #If we have new sensor ask for name description and add to api 
-
+    subdir = get_immediate_subdirectories("/sys/bus/w1/devices")
+    temperatureSensor = []
+    for dir in subdir:
+        if(dir != "w1_bus_master1"):
+            temperatureSensor.append(TemperatureSensor(dir))
+    while(1):
+        #display sensor temperature 
+        lcd.lcd_string(temperatureSensor[0].read(),1)
+        lcd.lcd_string(temperatureSensor[1].read(),2) 
+        
+        time.sleep(120)     
+        
+    #If we have new sensor ask for name description and add to api 
     #loop on temperatureSensor
-    	#Read temperature for each sensor 
-    	#Update push data in api for each sensor 
-    	#Display data on lcd screen
-    	#Check current profil data 
-    		#if bad temperature Work on fridge temperature 
-    		#Be carefull to anticipate temperature limit 
+        #Read temperature for each sensor 
+        #Update push data in api for each sensor 
+        #Display data on lcd screen
+        #Check current profil data 
+        #if bad temperature Work on fridge temperature 
+        #Be carefull to anticipate temperature limit
