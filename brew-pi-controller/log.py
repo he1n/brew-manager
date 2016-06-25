d51ab5ca18548b9a30a0b5213e7c545976bb1ca3
scaule <scaule@clever-age.com>
2016-06-25 13:59:19 +0200

    add thingspeek usage

---
+++ brew-pi-controller/log.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import httplib, urllib
+
+class Logger:
+
+    def __init__(self, apikey):
+        self.apikey = apikey
+    
+    def write(self,temp,field):
+        params = urllib.urlencode({field: temp, 'key':self.apikey})
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")
+        try:
+            conn.request("POST", "/update", params, headers)
+        except:
+            print "Api connection failed"
+    
--- brew-pi-controller/main.py
+++ brew-pi-controller/main.py
@@ -4,7 +4,7 @@ import os
 import time
 from lcd import Lcd
 from temperatureSensor import TemperatureSensor
-
+from log import Logger
 
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
@@ -13,7 +13,9 @@ def get_immediate_subdirectories(a_dir):
 if __name__ == "__main__":
     #Initialize lcd and test screen 
     lcd = Lcd()
-    
+    #Init Logger
+    logger = Logger("PF3I0333ITSWQ97W")
+
     #Get all sensor name 
     subdir = get_immediate_subdirectories("/sys/bus/w1/devices")
     temperatureSensor = []
@@ -23,15 +25,16 @@ if __name__ == "__main__":
     while(1):
         #display sensor temperature 
         lcd.lcd_string(temperatureSensor[0].read(),1)
-        lcd.lcd_string(temperatureSensor[1].read(),2) 
-        
-        time.sleep(120)     
-        
-    #If we have new sensor ask for name description and add to api 
-    #loop on temperatureSensor
-        #Read temperature for each sensor 
-        #Update push data in api for each sensor 
+        lcd.lcd_string(temperatureSensor[1].read(),2)
+        #Update push data in api for each sensor
+        #We have to push to thingspeek and local database
+        logger.write(temperatureSensor[0].read(),"field1")
+        logger.write(temperatureSensor[0].read(),"field2")
+        #if we have to control temperature
+        #If we have new sensor ask for name description and add to api
+
         #Display data on lcd screen
         #Check current profil data 
         #if bad temperature Work on fridge temperature 
         #Be carefull to anticipate temperature limit
+        time.sleep(120)
