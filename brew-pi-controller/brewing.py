49555750f66cbcbab2c22d91d03388a64d592990
scaule <scaule@clever-age.com>
2017-02-26 15:02:24 +0100

    update code for brewing system

--- brew-pi-controller/brewing.py
+++ brew-pi-controller/brewing.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     cartographyName = "brewing-cartography.yaml"
     temperaturePrecision = 2
     cartography = False
+    gpio = 11
     havetoleave = False
     timer = 0
     currentStep = 1
@@ -23,6 +24,7 @@ if __name__ == "__main__":
     timerStarted = False
     startTime = 0
     sleeptime = 0.1
+    GPIO.setup(gpionb, GPIO.OUT, initial = GPIO.HIGH)
     subdir = get_immediate_subdirectories("/sys/bus/w1/devices")
     temperatureSensor = []
     for dir in subdir:
@@ -41,31 +43,36 @@ if __name__ == "__main__":
     while(havetoleave != True):
     	currentTemperature = temperatureSensor[0].read()
     	print currentTemperature
-        break
     	#Check if we have to start the timer  
         if timerStarted != True and ( currentTemperature > (cartography.temperature - temperaturePrecision) and currentTemperature < (cartography.temperature + temperaturePrecision)) :
+            print "start timer "
             timerStarted = True
             start_time = time.time()
     	else:
     	    #Update time started we increment time 
     	    if(timerStarted):
+               print "up currentTime" 
     	       currentTime += (time.time() - start_time)
     	    
     	    #if time of current step is over we stop time and increment step   
     	    if currentTime >= cartography["step-" + str(currentStep)]['time'] :
+                print "step is over so we change step or quit "
     		    timerStarted = False
     		    currentTime = 0
     		    currentStep += 1
     		    #if we have no more steps we leave 
        		    if not ("step-" + str(currentStep)) in cartography:
-       			havetoleave = True
-       			break
+                    print "leave bacause we have no more space"
+       			    havetoleave = True
+       			    break
 
         #Check if we have to up temperature or not 
         if currentTemperature < cartography["step-" + str(currentStep)]['temperature']:
             # Start resistance
+            GPIO.setup(gpionb, GPIO.OUT, initial = GPIO.LOW)
             break 
         else:
+            GPIO.setup(gpionb, GPIO.OUT, initial = GPIO.HIGH)
             break
             # Stop Resistance	
 
